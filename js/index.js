3ae088e00927a84a156648dd3dada70d9fccd0fc
Doan Khiet Thanh <81890086+doankhietthanh@users.noreply.github.com>
2021-12-03 11:24:13 +0700

    up

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 const endPoint = "https://qr-server-191.herokuapp.com";
+// const endPoint = "http://127.0.0.1:3000";
+
 var socket = io(endPoint);
 fetch(endPoint + "/getQR")
   .then((res) => res.json())
@@ -9,25 +11,59 @@ fetch(endPoint + "/getQR")
 const personList = document.querySelector("#personList");
 let roleStatus;
 
-const row = (number, personID, name, role) => {
+const row = (number, personID, name, role, id, time) => {
   if (role == "Student") roleStatus = "status-student";
   else if (role == "Teacher") roleStatus = "status-teacher";
   return `<tr>
-    <td>${number}</td>
-    <td>${personID}</td>
-    <td>${name}</td>
-    <td><p class="status ${roleStatus}">${role}</p></td>
-    <td class="delete"><i class="fas fa-times-circle"></i></i></td>
-</tr>`;
-};
-
-const insertRow = (data) => {
-  personList.insertAdjacentHTML(
-    "beforeend",
-    row(++number, data.studentID, data.name, data.role, "", "")
-  );
+  <td>${number}</td>
+  <td>${personID}</td>
+  <td>${name}</td>
+  <td><p class="status ${roleStatus}">${role}</p></td>
+  <td><p class="">${time}</p></td>
+  <td class="delete"><i onclick="deleteScanner(${id})" class="fas fa-times-circle"></i></i></td>
+  </tr>`;
 };
 let number = 0;
+const deleteScanner = (id) => {
+  fetch(endPoint + "/deleteScanner", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id,
+    }),
+  })
+    .then((res) => res.json())
+    .then((result) => {
+      personList.innerHTML = "";
+      if (result.success) {
+        number = 0;
+        result.data.forEach((data) => {
+          insertRow(data);
+        });
+      }
+    });
+};
+
+const tbody = document.querySelector("tbody");
+const insertRow = (data) => {
+  const date = new Date(data.time);
+
+  personList.insertAdjacentHTML(
+    "beforeend",
+    row(
+      ++number,
+      data.studentID,
+      data.name,
+      data.role,
+      data.id,
+      date.toLocaleTimeString()
+    )
+  );
+  tbody.scrollTo(0, tbody.scrollHeight);
+};
 
 fetch(endPoint + "/getAllScanned")
   .then((res) => res.json())
@@ -42,30 +78,3 @@ fetch(endPoint + "/getAllScanned")
 socket.on("scan", (data) => {
   insertRow(data);
 });
-
-const showMenu = (toggleId, navbarId, bodyId) => {
-  const toggle = document.getElementById(toggleId),
-    navbar = document.getElementById(navbarId),
-    bodypadding = document.getElementById(bodyId);
-
-  if (toggle && navbar) {
-    toggle.addEventListener("click", () => {
-      // APARECER MENU
-      navbar.classList.toggle("show");
-      // ROTATE TOGGLE
-      toggle.classList.toggle("rotate");
-      // PADDING BODY
-      bodypadding.classList.toggle("expander");
-    });
-  }
-};
-showMenu("nav-toggle", "navbar", "body");
-
-// Change active link when clicked
-const linkColor = document.querySelectorAll(".nav-link");
-function colorLink() {
-  linkColor.forEach((l) => l.classList.remove("active"));
-  this.classList.add("active");
-}
-
-linkColor.forEach((l) => l.addEventListener("click", colorLink));
